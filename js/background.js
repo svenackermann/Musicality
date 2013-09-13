7b856ecb7d89bf5c4602bc3eec3131c713ac3f5f
Kyle Kamperschroer <kyle@kylek.me>
2013-09-13 11:25:02 -0700

    Some more basic progress

--- js/background.js
+++ js/background.js
@@ -1,18 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////
+// Constants
+/////////////////////////////////////////////////////////////////////////////
+
+// The location of the JSON full of players and patterns, relative to html dir
+var ALL_PLAYERS_JSON = "../json/all_players.json"
 
 /////////////////////////////////////////////////////////////////////////////
 // Member Variables
 /////////////////////////////////////////////////////////////////////////////
 
 // The id of the tab that was last seen currently playing music
-var mTabId = null;
+var mLastPlayingTabId = null; // TODO -- This needs to be stored locally, not here
+
+// The parsed ALL_PLAYERS_JSON
+var mAllPlayers = null;
+
+// A debug var for printing information to console
+var mDebug = true;
 
 /////////////////////////////////////////////////////////////////////////////
 // Functions
 /////////////////////////////////////////////////////////////////////////////
 
 // Find a tab that is currently playing music
-function FindTabPlayingMusic(callback){
+function FindTabPlayingMusic(){
 
+    if (mDebug){
+        console.log("background.js::FindTabPlayingMusic(" + callback + ")");
+    }
+    
     // Cycle through each tab
 
     // TODO -- Look for a tab currently playing music
@@ -22,17 +38,45 @@ function FindTabPlayingMusic(callback){
 // Update the information displayed within the extension
 function UpdateInformation(){
 
-    // TODO -- See if the tab we had before is still playing music
-    // If it is, then get the information from it.
-    // If it is not, then see if any other tabs are.
-    //    Get information for whichever one is
-    // If none are, then show the information of the tab we have
+    if (mDebug){
+        console.log("background.js::UpdateInformation()");
+    }
 
+    // Have we already found a tab playing music?
+    if (mLastPlayingTabId != null){
+        // Is it still playing music?
+        if (IsPlayingMusic(mLastPlayingTabId)){
+            // Grab the different pieces from that tab
+            PopulateInformation(mLastPlayingTabId);
+            return;
+        }
+    }
+
+    // We need to find a tab playing music
+    mLastPlayingTabId = FindTabPlayingMusic();
+    if (mLastPlayingTabId != null){
+        // We've got one. Populate
+        PopulateInformation(mLastPlayingTabId);
+    }
+}
+
+// Populate the actual extension given the particular tab id
+function PopulateInformation(tabId){
+    // TODO
+}
+
+// Function to determine if a given tab is playing music
+function IsPlayingMusic(tabId){
+    // TODO
+    return false;
 }
 
 // Function to load the values from external JSON
-function LoadPlayersInformation(){
+function LoadPlayersInformation(playerName){
 
+    if (mDebug){
+        console.log("background.js::LoadPlayersInformation(" + playerName + ")");
+    }
     // TODO -- Load directly from JSON the patterns we need
 
     // TODO -- Load directly from JSON the div classes/ids for elements given the player
@@ -47,8 +91,18 @@ function LoadPlayersInformation(){
 // Once the document is ready, bind all of the functions.
 $(document).ready(function(){
 
+    // Immediately update our information
+    UpdateInformation();
+    
     // Deserialize the individual players JSON
-    // TODO -- Load high level json into memory
+    $.getJSON(ALL_PLAYERS_JSON, function(data) {
+        mAllPlayers = data;
+
+        if (mDebug){
+            console.log(mAllPlayers);
+        }
+
+    });
 
     //Update our information once every second.
     window.setInterval(function() {
