05c207c437fc4a4ab475c8f761b3676cc510de3e
Kyle Kamperschroer <kyle@kylek.me>
2013-10-31 12:03:58 -0700

    Fix some tiny things

--- js/popup.js
+++ js/popup.js
@@ -396,10 +396,10 @@ $(document).ready(function(){
     // Immediately update our information
     UpdateInformation();
     
-    //Update our information once every quarter second.
+    //Update our information once every half second.
     window.setInterval(function() {
         UpdateInformation();
-    }, 1000)
+    }, 500)
 
     // Get the clickable elements ready!
 
