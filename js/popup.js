e828b326fb7cfab19548510dd27e174b231bb0e0
U-U300S\Kyle <kyle@kylek.me>
2013-12-22 12:08:39 -0800

    Don't display anything in the popup unless we have a track

--- js/popup.js
+++ js/popup.js
@@ -47,26 +47,6 @@ function PopulateInformation(){
         console.log("popup.js::PopulateInfo -- artist: " + artist);
     }
 
-    // Get the artist element
-    var artistElement = $("#artist");
-    if (artist != null){
-        artistElement.text(artist);
-
-        // Check if we need to marquee it
-        if(artistElement.get(0).scrollWidth > artistElement.width()){
-            // Turn it into a marquee
-            artistElement.attr('direction', 'right');
-            artistElement.attr('scrollamount', '1');
-        }else{
-            artistElement.attr('scrollamount', '0');
-            artistElement.attr('direction', 'left');
-        }
-    }else{
-        // No artist. Update text appropriately
-        artistElement.text("");
-    }
-
-
     // Get the track from the background
     var track = mBackground.mTrack;
 
@@ -152,7 +132,189 @@ function PopulateInformation(){
 
         // Display the name of the player that is playing
         playerNameElement.text(mPlayerDetails.name);
+
+        // Get the artist element
+        var artistElement = $("#artist");
+        if (artist != null && artist != ""){
+            artistElement.text(artist);
+
+            // Check if we need to marquee it
+            if(artistElement.get(0).scrollWidth > artistElement.width()){
+                // Turn it into a marquee
+                artistElement.attr('direction', 'right');
+                artistElement.attr('scrollamount', '1');
+            }else{
+                artistElement.attr('scrollamount', '0');
+                artistElement.attr('direction', 'left');
+            }
+        }else{
+            // No artist. Update text appropriately
+            artistElement.text("");
+        }
+
+        // Get the album art from the background
+        var art_url = mBackground.mArtUrl;
         
+        // Log it if we've found the art
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- art URL: " + art_url);
+        }
+
+        var artElement = $("#art");
+        
+        if (art_url && art_url != null && art_url != ""){
+            // Update the art to display the now playing art
+            artElement.attr("src", art_url);
+        }else{
+            // Not found, so revert it to the empty art
+            $("#art").attr("src", "/images/empty.png");
+        }
+
+        // Get the current time from the player
+        var current_time = mBackground.GetTimeStringForMilliseconds(mBackground.mCurrentTime);
+        
+        // Log it if we've found the current time
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- current time: " + current_time);
+        }
+
+        // Get the element in our extension
+        var curTimeElement = $("#cur_time");
+
+        // Update the info
+        if (current_time != null && current_time != "" && mBackground.mCurrentTime > 0){
+            curTimeElement.text(current_time + "/");
+        }else{
+            curTimeElement.text("");
+        }
+
+        // Get the total time
+        var total_time = mBackground.GetTimeStringForMilliseconds(mBackground.mTotalTime);
+
+        // Log it if we've found the total time
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- total time: " + total_time);
+        }
+
+        // Get the total time element
+        var totalTimeElement = $("#total_time");
+
+        // Update the info
+        if (total_time != null && total_time != "" && mBackground.mTotalTime > 0){
+            totalTimeElement.text(total_time);
+        }else{
+            totalTimeElement.text("");
+        }
+
+        // Get is playing from background
+        var playing = mBackground.mIsPlaying;
+
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is playing: " + playing);
+        }
+
+        // Get the element
+        var playPauseElement = $("#play_pause");
+
+        // Set the class of the element
+        if (playing){
+            playPauseElement.attr("class", "pause");
+        }else{
+            playPauseElement.attr("class", "play");
+        }
+
+        // Get whether or not it's shuffled
+        var shuffled = mBackground.mIsShuffled;
+
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is shuffled: " + shuffled);
+        }
+
+        // Get the element
+        var shuffleElement = $("#shuffle_button");
+
+        if (shuffled){
+            shuffleElement.attr("class", "shuffle_on");
+        }else{
+            shuffleElement.attr("class", "shuffle_off");
+        }
+        
+        // Get whether or not repeat is off
+        var repeat_off = mBackground.mIsRepeatOff;
+        
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is repeat off: " + repeat_off);
+        }
+
+        // Get the element
+        if (repeat_off){
+            $("#repeat_button").attr("class", "repeat_off");
+        }
+
+        // Get whether or not repeat is on (1)
+        var repeat_one = mBackground.mIsRepeatOne;
+
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is repeat one: " + repeat_one);
+        }
+
+        // Get the element
+        if (repeat_one){
+            $("#repeat_button").attr("class", "repeat_one");
+        }
+
+        // Get whether or not it's repeat all
+        var repeat_all = mBackground.mIsRepeatAll;
+
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is repeat all: " + repeat_all);
+        }
+
+        // Get the element
+        if (repeat_all){
+            $("#repeat_button").attr("class", "repeat_all");
+        }
+
+        // Get the thumbs up state
+        var thumbed_up = mBackground.mIsThumbedUp;
+        
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is thumbed up: " + thumbed_up);
+        }
+
+        // Get the thumbs up element
+        var thumbsUpElement = $("#thumbs_up_button");
+
+        // Toggle it
+        if (thumbed_up){
+            thumbsUpElement.attr("class", "thumbs_up_on");
+        }else{
+            thumbsUpElement.attr("class", "thumbs_up_off");
+        }
+
+        // Get the thumbed down state
+        var thumbed_down = mBackground.mIsThumbedDown;
+
+        // Log whatever we have got
+        if (mDebug){
+            console.log("popup.js::PopulateInfo -- is thumbed down: " + thumbed_down);
+        }
+
+        // Get the thumbs up element
+        var thumbsDownElement = $("#thumbs_down_button");
+
+        // Toggle it
+        if (thumbed_down){
+            thumbsDownElement.attr("class", "thumbs_down_on");
+        }else{
+            thumbsDownElement.attr("class", "thumbs_down_off");
+        }
     }else{
         // Looks like we have to disable some buttons
         playPauseElement.css("opacity", ".1");
@@ -166,171 +328,6 @@ function PopulateInformation(){
 
         // Tell the user nothing is playing
         trackElement.text("Play a song.");
-    }
-
-
-    // Get the album art from the background
-    var art_url = mBackground.mArtUrl;
-    
-    // Log it if we've found the art
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- art URL: " + art_url);
-    }
-
-    var artElement = $("#art");
-    
-    if (art_url && art_url != null && art_url != ""){
-        // Update the art to display the now playing art
-        artElement.attr("src", art_url);
-    }else{
-        // Not found, so revert it to the empty art
-        $("#art").attr("src", "/images/empty.png");
-    }
-
-    // Get the current time from the player
-    var current_time = mBackground.GetTimeStringForMilliseconds(mBackground.mCurrentTime);
-    
-    // Log it if we've found the current time
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- current time: " + current_time);
-    }
-
-    // Get the element in our extension
-    var curTimeElement = $("#cur_time");
-
-    // Update the info
-    if (current_time != null && current_time != "" && mBackground.mCurrentTime > 0){
-        curTimeElement.text(current_time + "/");
-    }else{
-        curTimeElement.text("");
-    }
-
-    // Get the total time
-    var total_time = mBackground.GetTimeStringForMilliseconds(mBackground.mTotalTime);
-
-    // Log it if we've found the total time
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- total time: " + total_time);
-    }
-
-    // Get the total time element
-    var totalTimeElement = $("#total_time");
-
-    // Update the info
-    if (total_time != null && total_time != "" && mBackground.mTotalTime > 0){
-        totalTimeElement.text(total_time);
-    }else{
-        totalTimeElement.text("");
-    }
-
-    // Get is playing from background
-    var playing = mBackground.mIsPlaying;
-
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is playing: " + playing);
-    }
-
-    // Get the element
-    var playPauseElement = $("#play_pause");
-
-    // Set the class of the element
-    if (playing){
-        playPauseElement.attr("class", "pause");
-    }else{
-        playPauseElement.attr("class", "play");
-    }
-
-    // Get whether or not it's shuffled
-    var shuffled = mBackground.mIsShuffled;
-
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is shuffled: " + shuffled);
-    }
-
-    // Get the element
-    var shuffleElement = $("#shuffle_button");
-
-    if (shuffled){
-        shuffleElement.attr("class", "shuffle_on");
-    }else{
-        shuffleElement.attr("class", "shuffle_off");
-    }
-    
-    // Get whether or not repeat is off
-    var repeat_off = mBackground.mIsRepeatOff;
-    
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is repeat off: " + repeat_off);
-    }
-
-    // Get the element
-    if (repeat_off){
-        $("#repeat_button").attr("class", "repeat_off");
-    }
-
-    // Get whether or not repeat is on (1)
-    var repeat_one = mBackground.mIsRepeatOne;
-
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is repeat one: " + repeat_one);
-    }
-
-    // Get the element
-    if (repeat_one){
-        $("#repeat_button").attr("class", "repeat_one");
-    }
-
-    // Get whether or not it's repeat all
-    var repeat_all = mBackground.mIsRepeatAll;
-
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is repeat all: " + repeat_all);
-    }
-
-    // Get the element
-    if (repeat_all){
-        $("#repeat_button").attr("class", "repeat_all");
-    }
-
-    // Get the thumbs up state
-    var thumbed_up = mBackground.mIsThumbedUp;
-    
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is thumbed up: " + thumbed_up);
-    }
-
-    // Get the thumbs up element
-    var thumbsUpElement = $("#thumbs_up_button");
-
-    // Toggle it
-    if (thumbed_up){
-        thumbsUpElement.attr("class", "thumbs_up_on");
-    }else{
-        thumbsUpElement.attr("class", "thumbs_up_off");
-    }
-
-    // Get the thumbed down state
-    var thumbed_down = mBackground.mIsThumbedDown;
-
-    // Log whatever we have got
-    if (mDebug){
-        console.log("popup.js::PopulateInfo -- is thumbed down: " + thumbed_down);
-    }
-
-    // Get the thumbs up element
-    var thumbsDownElement = $("#thumbs_down_button");
-
-    // Toggle it
-    if (thumbed_down){
-        thumbsDownElement.attr("class", "thumbs_down_on");
-    }else{
-        thumbsDownElement.attr("class", "thumbs_down_off");
     }
 }
 
