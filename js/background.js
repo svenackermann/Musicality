d401253935c97068cf409a04082ba114adf0c571
U-U300S\Kyle <kyle@kylek.me>
2014-02-07 18:23:19 -0800

    Finish up welcome page

--- js/background.js
+++ js/background.js
@@ -916,7 +916,7 @@ function ProcessFirstRun(){
     });
 
     // Check if we've shown the user the current welcome page
-    chrome.storage.local.get('welcome_shown' function(result){
+    chrome.storage.local.get('welcome_shown', function(result){
         if (!result.welcome_shown){
             // Save off that we've shown it
             chrome.storage.local.set({'welcome_shown' : true}, function(){
