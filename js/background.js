792e3c92d9a8ed191178574ef8634939fb021084
Kyle Kamperschroer <kyle@kylek.me>
2014-02-07 16:54:39 -0800

    Some work for the new first-time-only welcome page

--- js/background.js
+++ js/background.js
@@ -914,6 +914,17 @@ function ProcessFirstRun(){
             }
         }
     });
+
+    // Check if we've shown the user the current welcome page
+    chrome.storage.local.get('welcome_shown' function(result){
+        if (!result.welcome_shown){
+            // Save off that we've shown it
+            chrome.storage.local.set({'welcome_shown' : true}, function(){
+                // Show the welcome page
+                chrome.tabs.create({'url' : chrome.extension.getURL('html/welcome.html')});
+            });
+        }
+    });
 }
 
 /////////////////////////////////////////////////////////////////////////////
